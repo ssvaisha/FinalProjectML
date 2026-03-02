bd099e45b7b06defbffe9c0e9e8fc79f5842aa77
ssvaisha <ssvaisha@ucsc.edu>
2026-03-01 16:21:29 -0800

    added the data analysis of train data

--- model.py
+++ model.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from torchvision import datasets, transforms
 from PIL import Image 
-from pathlib import Path
+import os
 
 import matplotlib.pyplot as plt
 from torchvision import datasets, transforms
@@ -23,7 +23,6 @@ for split in ["train", "test", "val"]:
     plot_index = 1
     
     for class_idx, class_name in enumerate(class_names):
-        
         # Get first 10 images for this class
         class_indices = [i for i, (_, label) in enumerate(dataset.samples) if label == class_idx][:10]
         
@@ -47,4 +46,29 @@ for split in ["train", "test", "val"]:
     plt.tight_layout()
     plt.show()
 
-         
+#data analysis
+train_dataset = datasets.ImageFolder("archive/train")
+test_dataset = datasets.ImageFolder("archive/test")
+val_dataset= datasets.ImageFolder("archive/val")
+class_types = ['COVID19', 'NORMAL','PNEUMONIA','TURBERCULOSIS']
+
+amount_data = {}
+for class_name in train_dataset.classes:
+    class_dir = os.path.join('archive/train',class_name)
+    count = len([folder_name for folder_name in os.listdir(class_dir) if os.path.isfile(os.path.join(class_dir,folder_name))])
+    
+    amount_data[str(class_name)] = count
+
+
+
+print(amount_data)
+
+fig, ax = plt.subplots()
+names = amount_data.keys()
+nums = amount_data.values()
+ax.bar(range(len(names)),nums, tick_label = names)
+ax.set_ylabel("number of images")
+ax.set_xlabel("type of data")
+
+
+plt.show()
