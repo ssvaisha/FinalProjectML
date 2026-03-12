b556756b040b291ba69fcb5e7a04ceee339f1182
ssvaisha <ssvaisha@ucsc.edu>
2026-03-12 13:02:11 -0700

    commented my parts of the code

--- model.py
+++ model.py
@@ -51,13 +51,19 @@ transform_show = transforms.Compose([
     v2.Resize((224,224)),
 ])
 
-for cat in ["train", "test", "val"]:   
+# this for loop and everything in it is to display 100 images from add folders containing images
+#for each catagory
+for cat in ["train", "test", "val"]:
+    #we link our data
     dataset = datasets.ImageFolder(f"archive_smallset/{cat}", transform=transform_show)
+    #get our class names
     class_names = dataset.classes
     
+    #make a figure to place images on
     plt.figure(figsize=(20, 12))
     plot_index = 1
     
+    #for every class in each catagory 
     for class_index, class_name in enumerate(class_names):
         class_indices = [i for i, (_, label) in enumerate(dataset.samples) if label == class_index][:25]
         
@@ -81,13 +87,14 @@ for cat in ["train", "test", "val"]:
             plot_index += 1
     
     plt.tight_layout()
+    #show all the images
     plt.show()
 
 #data analysis
 train_dataset = datasets.ImageFolder("archive_smallset/train")
 
 amount_data = {}
-
+#this is to count our data
 for class_type in train_dataset.classes:
     class_directive = os.path.join('archive_smallset/train',class_type)
 
@@ -136,24 +143,33 @@ print_one_batch(train_loader, "TRAIN")
 print_one_batch(val_loader, "VAL")
 print_one_batch(test_loader, "TEST")
 
+#ConvModel class holds our model
 class ConvModel(nn.Module):
     def __init__(self):
         super().__init__()
-
+        #start with three convolution layers. Images are 3x1x1. we start at 3 frames and end at 6
         self.conv1 = nn.Conv2d(3,6,3,1,1)
+        #go from 6 frames to 16
         self.conv2 = nn.Conv2d(6,16,3,1,1)
+        #16 to 48 frames
         self.conv3 = nn.Conv2d(16,48,3,1,1)
 
         self.pool = nn.MaxPool2d(2,2)
+        #activation function of choice is ReLU
         self.relu = nn.ReLU()
 
+        #now our linear layers which take 48 * 224 *224 (48 frames from the convolutional layers times pixel length and width of image)
         self.fc1 = nn.Linear(37632,1000)
+        #drop out drops some of our data to prevent overfitting
         self.dropout = nn.Dropout(0.3)
+        #second linear layer takes 1000 nodes and ends with 4 nodes representing our four catagories
         self.fc2 = nn.Linear(1000,4)
+        #flatten to one dimension
         self.flatten = nn.Flatten()
 
+    #this is our foward pass on the model
     def forward(self,x):
-
+        #after each convolutional layer we pass it through relu and then pool the value
         x = self.relu(self.conv1(x))
         x = self.pool(x)
 
@@ -162,43 +178,51 @@ class ConvModel(nn.Module):
         
         x = self.relu(self.conv3(x))
         x = self.pool(x)
-
+        #the we flatten our 2d data to 1d
         x = x.flatten(start_dim =1)
-
+        #we put the first linear layer through relu and then drop out some values
         x = self.relu(self.fc1(x))
         x = self.dropout(x)
-
+        #then output our prediction(raw number) from the second linear layer
         output = self.fc2(x)
 
         return output
 
 model = ConvModel().to(device)
 
+#after three epochs we notice training loss goes down but validation loss does not indicating overfitting after 3 epochs
 NUM_Epoch = 3
+#we choice optimizer Adam and learning rate of 0.001 (standard recommended)
 optimizer = optim.Adam(model.parameters(), lr=0.001)
+#cross entropy loss because binary wouldn't work for 4 catagories and MSE or RMSE are for regression
 loss_fn = nn.CrossEntropyLoss()
 
 train_losses = []
 val_losses = []
 val_accuracies = []
 
-#training loop
+#first a for loop to run for all epochs
 for epoch in range(NUM_Epoch):
+    #set our model to train
     model.train()
     running_train_loss = 0.0
     train_total = 0
-
+    #take our inputs and outputs from our data loader 
     for train_inputs, train_outputs in train_loader:
         train_inputs = train_inputs.to(device)
         train_outputs = train_outputs.to(device)
 
+        #pass our inputs through the model and get train_prediction outputs
         train_preds = model(train_inputs)
+        #use our loss funtion to see the error in our prediction
         loss = loss_fn(train_preds,train_outputs)
 
+        #this is the learning step our optimizer moves around the weights to find better fitting patterns
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
+        #calculate our running loss
         running_train_loss += loss.item() * train_inputs.size(0)
         train_total += train_inputs.size(0)
 
