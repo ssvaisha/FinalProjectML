21c95ac2843a64554615714f2dc870edcbe6adda
Divya9246 <amitadivyasree@gmail.com>
2026-03-14 23:47:17 -0700

    comments made

--- model.py
+++ model.py
@@ -15,7 +15,8 @@ from torch.optim.lr_scheduler import ExponentialLR as ExpLR
 
 from sklearn.metrics import f1_score, confusion_matrix, ConfusionMatrixDisplay, classification_report
 
-#like we used seed in midterm
+# like we used seed in midterm
+# So that random numbers are generated in the same way each run as much as possible
 SEED = 42
 random.seed(SEED)
 np.random.seed(SEED)
@@ -23,7 +24,8 @@ torch.manual_seed(SEED)
 if torch.cuda.is_available():
     torch.cuda.manual_seed_all(SEED)
 
-
+# checking whether gpu is available
+# if yes,we use cuda because it is much faster
 if torch.cuda.is_available():
     device = 'cuda'
     print("cuda is available. using gpu.")
@@ -32,94 +34,123 @@ else:
     device ='cpu'
     print("cuda not available. using cpu.")
 
+# transforms for training data
+# these are applied only to train data so the model sees slightly varied images while learning
+# this helps the model generalize better instead of memorizing exact images
 transform = transforms.Compose([
-    transforms.ToTensor(),
-    v2.Resize((224,224)),
-    transforms.ColorJitter(brightness=0.2, contrast=0.2),   
-    transforms.GaussianBlur(kernel_size=3),                 
-    transforms.Normalize(mean=[0.5,0.5,0.5], std=[0.5,0.5,0.5]),
-    transforms.RandomHorizontalFlip(0.15),
+    transforms.ToTensor(),      # converts image into tensor so pytorch can use it
+    v2.Resize((224,224)),       # makes every image the same size that is 224x224      
+    transforms.ColorJitter(brightness=0.2, contrast=0.2),        # slightly changes brightness and contrast
+    transforms.GaussianBlur(kernel_size=3),     # slightly changes brightness and contrast                
+    transforms.Normalize(mean=[0.5,0.5,0.5], std=[0.5,0.5,0.5]),        # scales pixel values
+    transforms.RandomHorizontalFlip(0.15),       # randomly flips image horizontally with 15% probability     
     ])
 
+# transforms for validation and test data
+# we do not add random changes here because we want the evaluation to be consistent
 transform_eval = transforms.Compose([
-    transforms.ToTensor(),
-    v2.Resize((224,224)),
-    transforms.Normalize(mean=[0.5,0.5,0.5], std=[0.5,0.5,0.5]),
+    transforms.ToTensor(),      # convert image to tensor again      
+    v2.Resize((224,224)),       # resize image
+    transforms.Normalize(mean=[0.5,0.5,0.5], std=[0.5,0.5,0.5]),        #scales pixel values
 ])
 
 transform_show = transforms.Compose([
-    transforms.ToTensor(),
-    v2.Resize((224,224)),
+    transforms.ToTensor(),      # convert image to tensor again
+    v2.Resize((224,224)),       # resize image
 ])
 
 # this for loop and everything in it is to display 100 images from add folders containing images
-#for each catagory
+# for each catagory
 for cat in ["train", "test", "val"]:
-    #we link our data
+    # we link our data
     dataset = datasets.ImageFolder(f"archive_smallset/{cat}", transform=transform_show)
-    #get our class names
+    # get our class names
     class_names = dataset.classes
     
-    #make a figure to place images on
+    # make a figure to place images on
     plt.figure(figsize=(20, 12))
     plot_index = 1
     
-    #for every class in each catagory 
+    # for every class in each catagory 
     for class_index, class_name in enumerate(class_names):
+        # get the first 25 indices belonging to this class
         class_indices = [i for i, (_, label) in enumerate(dataset.samples) if label == class_index][:25]
         
+        # loop through those selected images
         for idx in class_indices:
+
+            # dataset[idx] returns the transformed image and its label
             img, label = dataset[idx]
             
             # rows = number of classes
             # cols = 25 images per class
             plt.subplot(len(class_names), 25, plot_index)
+
+            # we used permute to change tensor shape from (C,H,W) to (H,W,C) for matplotlib display
             plt.imshow(img.permute(1, 2, 0).clamp(0, 1)) 
-            #we added .clamp because it forces every pixel to stay in between 0 and 1 
-            #because it was showing an error while plotting in matplot when running the code
+            # we added .clamp because it forces every pixel to stay in between 0 and 1 
+            # because it was showing an error while plotting in matplot when running the code
+
             plt.title(cat)
-            #this is to label the y values so its like a bar chart
+            # title tells us whether images are from train, test, or val
+            # label first image in each row with the class name
+            # this makes the figure easier to read
             if plot_index % 25 == 1:
                 plt.ylabel(class_name, fontsize=12)
             
+            # we also remove x and y tick marks because they are not useful for the image (all this is only for better viewing of the image)
             plt.xticks([])
             plt.yticks([])
             
+            # now we move to next subplot position
             plot_index += 1
     
+    # adjusting spacing so plots do not overlap
     plt.tight_layout()
-    #show all the images
+    # show all the images
     plt.show()
 
-#data analysis
+# Data Analysis
+# loading training dataset without transform because here we only want class names and file counts
 train_dataset = datasets.ImageFolder("archive_smallset/train")
 
+# dictionary to store number of images in each class
 amount_data = {}
-#this is to count our data
+
+# this is to count our data
+# so we loop through each class folder
 for class_type in train_dataset.classes:
     class_directive = os.path.join('archive_smallset/train',class_type)
 
+    #we are counting how many actual files are in the folder
     count = len([
         folder_name for folder_name in os.listdir(class_directive) 
         if os.path.isfile(os.path.join(class_directive,folder_name))
     ])
     
+    # storing the count in the dictionary
     amount_data[str(class_type)] = count
 
 print(amount_data)
 
+# we are now making a bar graph of the distribution so we can clearly visualize if the model would become biased
 fig, ax = plt.subplots()
 
+# class name on x axis
 names = amount_data.keys()
+
+# counts on y axis
 nums = amount_data.values()
 
 ax.bar(range(len(names)),nums, tick_label = names)
 ax.set_ylabel("number of images")
 ax.set_xlabel("type of data")
 
+# show the bar chart
 plt.show()
 
 # Looping over each DataLoader
+# this is to print one batch from a dataloader just to see if the shape is okay and if the labels are good 
 def print_one_batch(loader, split_name):
     print("\n----", split_name, "----")
     for inputs, outputs in loader:
@@ -129,51 +160,60 @@ def print_one_batch(loader, split_name):
         print("first image first 10 pixel values:", inputs[0].flatten()[:10].tolist())
         break 
 
-
+# now we creating datasets with proper transforms
 train_dataset = datasets.ImageFolder("archive_smallset/train", transform=transform)
 val_dataset   = datasets.ImageFolder("archive_smallset/val",   transform=transform_eval)
 test_dataset  = datasets.ImageFolder("archive_smallset/test",  transform=transform_eval)
 
-# Creating DataLoaders
+# Creating DataLoaders to load data in batches
 batch_size = 32
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 val_loader   = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
 test_loader  = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
+# just to check if everything is working fine we printed one batch
 print_one_batch(train_loader, "TRAIN")
 print_one_batch(val_loader, "VAL")
 print_one_batch(test_loader, "TEST")
 
-#ConvModel class holds our model
+# ConvModel class holds our model
 class ConvModel(nn.Module):
     def __init__(self):
         super().__init__()
-        #start with three convolution layers. Images are 3x1x1. we start at 3 frames and end at 6
+        # start with three convolution layers. Images are 3x1x1. we start at 3 frames and end at 6
+        # 3 because RGB
         self.conv1 = nn.Conv2d(3,6,3,1,1)
         
-        #go from 6 frames to 16
+        # go from 6 frames to 16
         self.conv2 = nn.Conv2d(6,16,3,1,1)
         
-        #16 to 48 frames
+        # 16 to 48 frames
         self.conv3 = nn.Conv2d(16,48,3,1,1)
         
-        
+        # max pooling with 2x2 window and stride 2
+        # this halves image width and height each time
         self.pool = nn.MaxPool2d(2,2)
-        #activation function of choice is ReLU
+        # activation function of choice is ReLU
         self.relu = nn.ReLU()
 
-        #now our linear layers which take 48 * 28 *28 (48 frames from the convolutional layers times pixel length and width of image after maxpool)
+        # after 3 rounds of pooling:
+        # 224 -> 112 -> 56 -> 28
+        # so final tensor size is 48 x 28 x 28
+        # flattened size = 48*28*28 = 37632
         self.fc1 = nn.Linear(37632,1000)
-        #drop out drops some of our data to prevent overfitting
+
+        # drop out drops some of our data to prevent overfitting
         self.dropout = nn.Dropout(0.3)
-        #second linear layer takes 1000 nodes and ends with 4 nodes representing our four catagories
+
+        # second linear layer takes 1000 nodes and ends with 4 nodes representing our four catagories
         self.fc2 = nn.Linear(1000,4)
-        #flatten to one dimension
+
+        # flatten to one dimension
         self.flatten = nn.Flatten()
 
-    #this is our foward pass on the model
+    # this is our forward pass on the model (how input moves through network)
     def forward(self,x):
-        #after each convolutional layer we pass it through relu and then pool the value
+        # after each convolutional layer we pass it through relu and then pool the value
         x = self.relu(self.conv1(x))
        
         x = self.pool(x)
@@ -187,60 +227,66 @@ class ConvModel(nn.Module):
         x = self.pool(x)
 
     
-        #the we flatten our 2d data to 1d
+        # the we flatten our 2d data to 1d
+        # flatten from shape [batch, 48, 28, 28] to [batch, 37632]
         x = x.flatten(start_dim =1)
-        #we put the first linear layer through relu and then drop out some values
+
+        # we put the first linear layer through relu and then drop out some values
         x = self.relu(self.fc1(x))
+        # dropout is only for training
         x = self.dropout(x)
-        #then output our prediction(raw number) from the second linear layer
+        # then output our prediction(raw number) from the second linear layer
         output = self.fc2(x)
 
         return output
 
 model = ConvModel().to(device)
 
-#after three epochs we notice training loss goes down but validation loss does not indicating overfitting after 3 epochs
+# after three epochs we notice training loss goes down but validation loss does not indicating overfitting after 3 epochs
 NUM_Epoch = 3
-#we choice optimizer Adam and learning rate of 0.001 (standard recommended)
+# we choice optimizer Adam and learning rate of 0.001 (standard recommended)
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-#cross entropy loss because binary wouldn't work for 4 catagories and MSE or RMSE are for regression
+# cross entropy loss because binary wouldn't work for 4 catagories and MSE or RMSE are for regression
 loss_fn = nn.CrossEntropyLoss()
 
+# lists to store results for plotting later
 train_losses = []
 val_losses = []
 val_accuracies = []
 
-#first a for loop to run for all epochs
+# Training and Validation loop
+# first a for loop to run for all epochs
 for epoch in range(NUM_Epoch):
-    #set our model to train
+    # set our model to train
     model.train()
     running_train_loss = 0.0
     train_total = 0
-    #take our inputs and outputs from our data loader 
+    # take our inputs and outputs from our data loader 
     for train_inputs, train_outputs in train_loader:
+        # move batch to gpu/cpu
         train_inputs = train_inputs.to(device)
         train_outputs = train_outputs.to(device)
 
-        #pass our inputs through the model and get train_prediction outputs
+        # pass our inputs through the model and get train_prediction outputs
         train_preds = model(train_inputs)
-        #use our loss funtion to see the error in our prediction
+        # use our loss funtion to see the error in our prediction
         loss = loss_fn(train_preds,train_outputs)
 
-        #this is the learning step our optimizer moves around the weights to find better fitting patterns
+        # this is the learning step our optimizer moves around the weights to find better fitting patterns
+        optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        optimizer.zero_grad()
         
-
-        #calculate our running loss
+        #c alculate our running loss
         running_train_loss += loss.item() * train_inputs.size(0)
         train_total += train_inputs.size(0)
 
+    # this is the average training loss across all training examples
     avg_train_loss = running_train_loss / train_total
     train_losses.append(avg_train_loss) 
 
-    #validation
+    # Validation
     model.eval()
     correct = 0
     total = 0
@@ -248,10 +294,13 @@ for epoch in range(NUM_Epoch):
 
     with torch.no_grad():
         for val_inputs, val_outputs in val_loader:
+            # move validation batch to gpu/cpu
             val_inputs = val_inputs.to(device)
             val_outputs = val_outputs.to(device)
 
             val_preds = model(val_inputs)
+
+            # calculating validation loss
             loss = loss_fn(val_preds, val_outputs)
             running_val_loss += loss.item() * val_inputs.size(0)
 
@@ -259,31 +308,42 @@ for epoch in range(NUM_Epoch):
 
             total += val_outputs.size(0)
             correct += (predicted == val_outputs).sum().item()
-
+    
+    # this is validation accuracy
     val_accuracy = correct/total
+
+    # average validation loss
     avg_val_loss = running_val_loss / total
 
+    # we are storing these values for graphing
     val_losses.append(avg_val_loss)
     val_accuracies.append(val_accuracy)
 
+    # print results for this epoch
     print(f"Epoch {epoch} Train Loss: {avg_train_loss}")
     print(f"Epoch {epoch} Validation Loss: {avg_val_loss}")
     print(f"Epoch {epoch} Validation Accuracy: {val_accuracy}")
 
-#testing
+# Testing
+# after training is complete, we can now evaluate on unseen test data
 model.eval()
 correct = 0
 total = 0
 test_loss = 0.0
+# storing all true labels and predictions for f1 score and confusion matrix
 alltest_labels = []
 alltest_preds = []
 
 with torch.no_grad():
     for test_inputs, test_outputs in test_loader:
+        # move test batch to gpu/cpu
         test_inputs = test_inputs.to(device)
         test_outputs = test_outputs.to(device)
 
+        # move validation batch to gpu/cpu
         test_preds = model(test_inputs)
+
+        # calculating loss on this test batch
         loss = loss_fn(test_preds, test_outputs)
         test_loss += loss.item() * test_inputs.size(0)
 
@@ -291,10 +351,11 @@ with torch.no_grad():
         total += test_outputs.size(0)
         correct += (predicted == test_outputs).sum().item()
 
+        # we are saving all these labels and predictions
         alltest_labels.extend(test_outputs.cpu().tolist())
         alltest_preds.extend(predicted.cpu().tolist())
 
-test_accuracy = correct / total
+test_accuracy = correct / total # test accuracy 
 avgtest_loss = test_loss / total
 test_f1 = f1_score(alltest_labels, alltest_preds, average="weighted")
 
@@ -302,10 +363,14 @@ print(f"Test Accuracy: {test_accuracy}")
 print(f"Test Loss: {avgtest_loss}")
 print(f"Weighted F1 Score: {test_f1}")
 
+# confusion matrix shows how many examples from each true class were predicted as each class
 cm = confusion_matrix(alltest_labels, alltest_preds)
 vt = np.array(cm)
 print(vt)
+
 classes = ["COVID19","NORMAL","PNEUMONIA","TUBERCULOSIS"]
+
+# calculating precision and recall for each class
 for i, label in enumerate(classes):
     tp = vt[i,i]
     fp = vt[:,i].sum()-tp
@@ -316,7 +381,7 @@ for i, label in enumerate(classes):
 
     print(f"{label}: Precision: {precision} | Recall: {recall}")
 
-
+# plotting confusion matrix 
 disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=test_dataset.classes)
 disp.plot(cmap="Blues", values_format="d")
 plt.title("Confusion Matrix")
@@ -324,6 +389,7 @@ plt.show()
 
 epochs = range(NUM_Epoch)
 
+# plotting training and validation loss
 plt.figure(figsize=(8, 5))
 plt.plot(epochs, train_losses, marker="o", label="Train Loss")
 plt.plot(epochs, val_losses, marker="o", label="Validation Loss")
@@ -333,7 +399,7 @@ plt.title("Train and Validation Loss")
 plt.legend()
 plt.show()
 
-
+# plotting validation accuracy
 plt.figure(figsize=(8, 5))
 plt.plot(epochs, val_accuracies, marker="o", label="Validation Accuracy")
 plt.xlabel("Epoch")
